912239239317ca9106f47a78899f2be5b7d97efa
Lucas Merritt <merrittlj@protonmail.com>
2024-09-21 00:54:07 -0600

    add installation nix

---
+++ installation-cd-minimal-broadcom.nix
@@ -0,0 +1,9 @@
+{ config, ... }:
+{
+  imports = [ ./installation-cd-minimal.nix ];
+  
+  boot.initrd.kernelModules = [ "wl" ];
+  
+  boot.kernelModules = [ "kvm-intel" "wl" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+}
